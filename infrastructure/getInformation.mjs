dab52329e4b58023e258bf8b686f14b01927d6f3
jeremy <projeremylegendre@gmail.com>
2024-03-25 11:42:40 +0100

    base

--- infrastructure/getInformation.mjs
+++
@@ -1,7 +0,0 @@
-import { dataSet } from "../domaine/dataset.mjs";
-//import { Personnage } from "./Personnage";
-let getDataSet = (dataSet) => {
-   return dataSet;
-}
-let dataSetArray = getDataSet(dataSet);
-export { dataSetArray };
